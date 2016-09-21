55791cd47d973098185057964cd13788820961eb
TSG\wabbas <engrwasim006@gmail.com>
2016-09-21 07:49:57 -0700

    This is first commit

---
+++ AlliedBuildings/AlliedBuildings/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlliedBuildings.Startup))]
+namespace AlliedBuildings
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
