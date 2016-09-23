054eff271f260527757aa924d6489c24979e028c
TSG\wabbas <engrwasim006@gmail.com>
2016-09-23 04:26:49 -0700

    change name of column

--- AlliedBuildings/AlliedBuildings/Database/dbo.AspNetRoles.sql
+++ AlliedBuildings/AlliedBuildings/Database/dbo.AspNetRoles.sql
@@ -4,7 +4,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[AspNetRoles](
 	[Id] [nvarchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[Name] [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Name1] [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
  CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
